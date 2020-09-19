000bab14d2a59f7ba1089cbd56d670276cb5b6bb
Tomasz Buchta <tomasz.buchta@hotmail.com>
2020-09-19 17:44:02 +0200

    Add spec for missing path

--- spec/quadrocopter_spec.rb
+++ spec/quadrocopter_spec.rb
@@ -4,18 +4,27 @@ RSpec.describe QueadrocopterPath do
     subject(:quadrocopter_path) { described_class.new(transmitters: transmitters, start_point: start, end_point: destination).safe? }
     let(:start)       { {x: 10, y: 19} }
     let(:destination) { {x: 19, y: 14} }
-    context "with available path" do
+    let(:transmitters) {
+      [
+        { x: 6, y: 11, power: 4 },
+        { x: 8, y: 17, power: 3 },
+        { x: 19, y: 19, power: 2 },
+        { x: 19, y: 11, power: 4 },
+        { x: 15, y: 7, power: 6 },
+        { x: 12, y: 19, power: 4 }
+      ]
+    }
+    it { is_expected.to eq(true) }
+    context "without available path" do
       let(:transmitters) {
         [
-          { x: 6, y: 11, power: 4 },
-          { x: 8, y: 17, power: 3 },
-          { x: 19, y: 19, power: 2 },
-          { x: 19, y: 11, power: 4 },
-          { x: 15, y: 7, power: 6 },
-          { x: 12, y: 19, power: 4 }
+          { x: 1, y: 1, power: 4 },
+          { x: 100, y: 100, power: 10 }
         ]
       }
-      it { is_expected.to eq(true) }
+      let(:start)       { {x: 1, y: 1} }
+      let(:destination) { {x: 100, y: 100} }
+      it { is_expected.to eq(false) }
     end
     context "with single transmitter" do
       let(:start)       { {x: 10, y: 10} }
